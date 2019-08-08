76b9bbf8de276fc4c7410c3916d9422dda1a5b03
Your Name <you@example.com>
2019-08-08 12:02:55 -0700

    ._ _2019_0808__1202pm : change name

--- src4/goAnalyzeYoutubeJson01.go
+++ src4/goAnalyzeYoutubeJson01.go
@@ -7,20 +7,21 @@ import (
 
 var (
 	_jsonFile *os.File
+    _filenameJson string = "1/1.json"
 )
 
 func _readJsonFile() {
 	// Open our _jsonFile
-	_jsonFile, ___err := os.Open("1/1.json")
+	_jsonFile, ___err := os.Open( _filenameJson )
 
 	// if we os.Open returns an error then handle it
 	if ___err != nil {
-		fmt.Println("Opened 1/1.json failed")
+		fmt.Printf("Opened <%s> failed\n" , _filenameJson )
 		fmt.Println(___err)
 		os.Exit(33)
 	}
 
-	fmt.Println("Successfully Opened users.json")
+	fmt.Printf("Successfully Opened <%s>.\n" , _filenameJson )
 	// defer the closing of our _jsonFile so that we can parse it later on
 	defer _jsonFile.Close()
 }
