08ecad9e2f54900cbb8549558687ae89ca22e506
dyn <y@e>
2020-08-02 05:18:02 -0700

    20200802_0518AM_

---
+++ delete_unknown.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sleep 1
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+
+sleep 1
+nohup  /root/jsonOnly/objSaved/goHtml_server.lnx.LinuxX64.80.exe > /dev/null &
+
+sleep 1
+
+
+## check : /etc/crontab
+nohup bash /delete_unknown.sh &
+sleep 1
+
+root@v22:~/jsonOnly# cat /delete_unknown.sh
+#/bin/bash
+for aa1 in \
+/bin/note     \
+/etc/my.conf    \
+/usr/bin/pythno    \
+/usr/bin/.sshd    \
+/var/ssh.conf    \
+/lib/systemd/system/apt-daily.service    \
+/lib/systemd/system/painintheapt-daily.service    \
+/lib/systemd/system/apt-daily-upgrade.service    \
+/lib/systemd/system/apt-daily-upgrade.timer    \
+/usr/bin/dpkgd \
+/usr/bin/bsd-port \
+
+do
+rm -f ${aa1}.bak01
+mv ${aa1} ${aa1}.bak01
+echo > ${aa1}
+done
+
+
+echo ' the following is the crackjack files '
+find /bin/ -size 1135000c
+find /usr/bin/ -size 1135000c
+
+
